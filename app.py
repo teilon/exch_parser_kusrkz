5e44c9513dcc2dda2fd785426a1005afe82436cd
teilon <teiloncw@gmail.com>
2020-08-20 01:48:22 +0600

    fix post data

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 # main python file
 from flask import Flask
-# from parser import parse
 from parser_driver import parse
+import requests
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/parse')
 def start():
-    parse()
-    return 'Done!'
+    data = parse()
+
+    url = 'http://78.155.206.12/items'
+    headers = {'Content-type': 'application/json',
+               'Accept': 'text/plain',
+               'Content-Encoding': 'utf-8'}
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+
+    if response.status_code == 201:
+        return {'message': 'Success!'}
+    return {'message': 'An error has occurred.'}
 
 
 if __name__ == '__main__':
--- parser_driver.py
+++ parser_driver.py
@@ -6,7 +6,7 @@ def parse():
     target_url = 'https://kurs.kz/'
 
     html = get_target_html(target_url)
-    get_data(html)
+    return get_data(html)
 
 
 def get_target_html(url, useragent=None, proxy=None):
@@ -41,29 +41,13 @@ def get_data(html):
 
         phones = [tag.text.strip() for tag in tds[5].find_all('a', class_='phone')]
 
-        # bank_data = {
-        #     'handler_name': bank_name,
-        #     'money_type': money_type,
-        #     'name': currency_name,
-        #     'buy': buy,
-        #     'sale': sale
-        # }
-        # bank_data = {
-        #     'bank': bank_name,
-        #     'money_type': money_type,
-        #     'currency': currency_name,
-        #     'buy': buy,
-        #     'sale': sale,
-        #     'creation_date': datetime.utcnow()
-        # }
         usd_data = make_data(name, address, phones[-1], 'USD', usd_buy, usd_sale)
         eur_data = make_data(name, address, phones[-1], 'EUR', eur_buy, eur_sale)
         rub_data = make_data(name, address, phones[-1], 'RUB', rub_buy, rub_sale)
 
         data.extend([usd_data, eur_data, rub_data])
 
-    with open('tmp.txt', 'a') as f:
-        f.write('{}\n'.format(data))
+    return data
 
 
 def make_data(entity_name, entity_address, entity_phone, currency_name, buy, sale):
